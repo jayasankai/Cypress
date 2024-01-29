6cdc211e5d422b05a902a6ff973b2a9ec167eb47
jayasanka <61721893+jayasanka-ucsc@users.noreply.github.com>
2024-01-29 21:20:40 +1300

    test authentication

---
+++ Project06/cypress/e2e/auth.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="Cypress" />
+
+describe('Auth', () => {
+
+    beforeEach(() => {
+        // Setup test specific database
+        cy.task('seedDatabase');
+    });
+
+    it('should signup', () => {
+        cy.visit('/signup');
+        cy.get('[data-cy="auth-email"]').type('test@test.com');
+        cy.get('[data-cy="auth-password"]').type('123123');
+        cy.get('[data-cy="auth-submit"]').click();
+        cy.location('pathname').should('eq', '/takeaways');
+        cy.getCookie('__session').its('value').should('not.be.empty');
+    });
+
+    it('should login', () => {
+        cy.login();
+    });
+
+    it('should logout', () => {
+        cy.login();
+        
+        // Logout 
+        cy.contains('Logout').click();
+        cy.location('pathname').should('eq', '/');
+        cy.getCookie('__session').its('value').should('be.empty');
+    });
+});
--- Project06/cypress/e2e/takeaways.cy.js
+++ Project06/cypress/e2e/takeaways.cy.js
@@ -4,7 +4,6 @@ describe('Takeaways', () => {
   beforeEach(() => {
     // Setup test specific database
     cy.task('seedDatabase');
-  
   });
 
   it('should display a list of fetched takeaways', () => {
@@ -13,4 +12,19 @@ describe('Takeaways', () => {
     cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
 
   });
+
+  it('should add a new takeaway', () => {
+    cy.intercept('POST', '/takeaways/new*', 'success').as('createTakeaway');
+
+    cy.login();
+
+    cy.visit('/takeaways/new');
+    cy.get('[data-cy="title"]').type('NewTakeawayTitle');
+    cy.get('[data-cy="body"]').type('NewTakeawayContent');
+    cy.get('[data-cy="create-takeaway"]').click();
+
+    cy.wait('@createTakeaway').its('request.body').should('match', /NewTakeawayTitle.*NewTakeawayContent/);
+
+  });
+
 });
